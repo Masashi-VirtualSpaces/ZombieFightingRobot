a08a33cd7d24cad3cf2b66fcc1f83f4cdb81701b
Masashi Schafer <masashi@virtualspaces.io>
2017-04-25 16:18:24 -0700

    Test wheel

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -30,16 +30,19 @@ int main(){
   double CurrentDistance = 0;
 
   //Set up motor PWM's initialize them to 0 with range 0-100%
-  softPwmCreate(PWM_LEFT,0,100);
-  softPwmCreate(PWM_RIGHT,0,100);
+  if(softPwmCreate(PWM_LEFT,0,100)){
+    print("left wheel initialized\n");
+  }
+  if(softPwmCreate(PWM_RIGHT,0,100)){
+    prinf("right wheel initialized\n");
+  }
 
   //Wait for switch to be hit in order to start program.
   //pinMode()
   bool switchWait = true;
   printf("about to go into wait mode. Activate\n");
-
   while(switchWait){
-    if(!digitalRead(IN_SW_UTIL_1)){
+    if(digitalRead(IN_SW_UTIL_1)){
       switchWait = false;
       softPwmWrite(PWM_LEFT,0);
       softPwmWrite(PWM_RIGHT,0);
